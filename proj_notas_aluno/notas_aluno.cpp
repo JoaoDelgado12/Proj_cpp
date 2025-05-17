6de7632924ffdccf99f0c93a5883b3a489b50414
João Delgado <157933820+JoaoDelgado12@users.noreply.github.com>
2025-05-16 21:17:25 -0300

    Update notas_aluno.cpp com erro
    tem um erro na criação dos arquivos

--- proj_notas_aluno/notas_aluno.cpp
+++ proj_notas_aluno/notas_aluno.cpp
@@ -2,14 +2,15 @@
 #include <fstream>
 
 using namespace std;
-int TAM_MENU; //para facilitar alteração do codigo
 
 struct ALUNO {
 	char nome[100];
-	long cpf;
-	float notas[3];
-	float media;
-	bool status;
+	long matricula;
+};
+
+struct PROF {
+	char nome[100];
+	long cod_acesso;
 };
 
 void menu();
@@ -17,6 +18,7 @@ void menu();
 int perm_case(int &op, string frase);
 
 void add_aluno(ALUNO aluno, int &op);
+void add_professor(PROF prof, int &op);
 
 int main() {
 	bool perm = 1; // variavel para mandar o programa rodando
@@ -25,14 +27,23 @@ int main() {
 	while(perm) {
 		switch(opcao) {
 			case 0: {
-
 				menu();
 				cin >> opcao;
+				system("cls");
 				break;
 			}
-			case 1: {
+			case 1: {	
 				ALUNO aluno; // criando a variavel aluno
 				add_aluno(aluno, opcao); //função para adicionar aluno
+				break;        
+			}
+			case 2:{
+				PROF professor; // criando a variavel professor
+				add_professor(professor, opcao); //função para adicionar professor
+				break;
+			}
+			case 5:{
+				perm = 0;
 				break;
 			}
 		}
@@ -42,32 +53,25 @@ int main() {
 
 void menu() { // menu das opcoes
 	string array_menu[] = { // as opcoes do menu
-		"      Menu Principal",
-		"Adicionar dados de aluno",
-		"Excluir dados de aluno",
-		"Calcular Media de todos os alunos",
-		"Atualizar a situacao de acordo com as medias geradas",
-		"Exibir todos os alunos Reprovados",
-		"Exibir todos os alunos Aprovados",
-		"Arredondar notas (alunos com media >= 6 e media < 7) arredondar para 7",
+		"cadastrar dados de aluno",
+		"cadastrar dados de professor",
+		"Login - Aluno",
+		"Login - Professor",
 		"Sair"
 	};
-	//essa parte é só para eu não me preocupar com o tamnanho do array
-	TAM_MENU = 1;
-	for(int ind = 0; ind < TAM_MENU; ind++){
-		TAM_MENU++;
-	}
-	//para ter um formatação certinha na saida do console
-	for(int ind = 0; ind < TAM_MENU; ind++){
-		cout << ind << " - " << array_menu[ind] << endl;
-	}
 	
+	cout << "        Menu Principal" << endl;
+	int ind = 1;
+	for(string frase : array_menu ){
+		cout << ind << " - " << frase << endl;
+		ind++;
+	}
 }
 
 int perm_case(int &op, string frase) { // para perguntar a cada opcao se o usuario quer sair
 	char resp = ' ';
 	while(resp != 's' && resp != 'n') {
-		cout << frase;
+		cout << frase << endl;
 		cin >> resp;
 		if(resp == 's') {
 			system("cls");
@@ -87,34 +91,46 @@ int perm_case(int &op, string frase) { // para perguntar a cada opcao se o usuar
 
 void add_aluno(ALUNO aluno, int &op){
 	bool perm_local = 1;
-	ofstream aqr_aluno;
+	ofstream aqr_aluno("data_aluno.bin", ios::app | ios::binary);
 	
 	while(perm_local){	
 		// cadastrar o aluno
 		cout << "Qual o nome do aluno?" << endl;
 		cin >> ws;
 		cin.getline(aluno.nome, 100);
-		cout << "Qual o CPF do aluno?" << endl;
-		cin >> aluno.cpf;
-		cout << "Quais as notas do aluno?" << endl;
-		aluno.notas[3] = {};
-		for(int ind=0; ind < 3; ind++){
-			cin >> aluno.notas[ind];
-		}
-		aluno.media = 0;
-		aluno.status = 0;
+		cout << "Qual o matricula do aluno?" << endl;
+		cin >> aluno.matricula;
 		
 		aqr_aluno.write((char*) &aluno, sizeof(aluno)); //escrevendo em data_aluno.bin p cadastro
-		aqr_aluno.close();	
 		
 		system("cls");
-		perm_local = perm_case(op, "Voce que adicionar mais um aluno?");
+		perm_local = perm_case(op, "Voce que adicionar mais um aluno?(s/n)");
+		
 	}
+	aqr_aluno.close();
 }
+
+void add_professor(PROF prof, int &op){
+	bool perm_local = 1;
+	ofstream aqr_prof("data_prof.bin", ios::app | ios::binary);
 	
-
-
-
+	while(perm_local){	
+		// cadastrar o professor
+		cout << "Qual o nome do professor?" << endl;
+		cin >> ws;
+		cin.getline(prof.nome, 100);
+		cout << "Qual o codigo de cadastro do professor?" << endl;
+		cin >> prof.cod_acesso;
+		
+		aqr_prof.write((char*) &prof, sizeof(prof)); //escrevendo em data_prof.bin para cadastro
+			
+		
+		system("cls");
+		perm_local = perm_case(op, "Voce que adicionar mais um professor?(s/n)");
+		
+	}
+	aqr_prof.close();
+}
 
 
 
