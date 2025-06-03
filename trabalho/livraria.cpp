7ce7e883cab057f267fede3fee6c674f8573f704
João Delgado <157933820+JoaoDelgado12@users.noreply.github.com>
2025-06-03 19:32:30 -0300

    Update and rename livraria to livraria.cpp

---
+++ trabalho/livraria.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+using namespace std;
+
+bool perm_case(int &op, char ficar, char sair, string frase);
+
+//case 0
+void menu_principal();
+
+
+
+int main() {
+	bool perm = 1; // variavel para mandar o programa rodando
+	int opcao = 0; // variavel para guardar a escolha do usuario
+    
+    
+	while(perm) {
+		switch(opcao) {
+			case 0: {
+				menu_principal();
+				cin >> opcao;
+				break;
+			}
+			case 1:{
+			    break;
+			}
+			case 2:{
+				break;
+			}
+			case 3:{
+			    
+			    break;
+			}
+			case 4:{
+			    perm = perm_case(opcao, 'n', 's', "Certeza que quer sair ?(s/n)");
+			    break;
+			}
+			default:{
+			    cout << "Entrada inválida. \n por favor coloque uma entrada validade de 1 a 4. \n";
+				break;
+		    }
+		}
+	}
+}
+
+//menus
+void menu_principal() { // menu das opcoes
+	string array_menu[] = { // as opcoes do menu
+		"Cadastros",
+        "Empréstimo",
+        "Relatórios",
+        "Sair"
+	};
+	//essa parte é só para eu não me preocupar com o tamnanho do array
+	cout <<"      Menu Principal" << endl;
+	int ind = 1;
+	for(string frase  : array_menu){
+	    cout << ind << " - " << frase << endl;
+	}
+}
+
+//tools
+bool perm_case(int &op, char ficar, char sair, string frase){ // para perguntar a cada opcao se o usuario quer sair
+	char resp = ' ';
+	while(resp != ficar && resp != sair) {
+		cout << frase;
+		cin >> resp;
+		if(resp == ficar) {
+			system("cls");
+			return 1; // permanece na operacao
+		} else if(resp == sair) {
+			system("cls");
+			op = 0; // para ir ao menu
+			return 0; // para sair da operacao
+		} else {
+			cout << "Entrada invalida! \n Escreva (s) ou (n)";
+			system("pause");
+			system("cls");
+		}
+	}
+	return 0;
+}
